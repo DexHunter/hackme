897a06509adf57604724ad38fe8f3ad81ae894e6
Dex Hunter <dixingxu@gmail.com>
2018-01-28 15:53:34 +0800

    update out.sh

--- out.sh
+++ out.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "need to commit current branch"
+    exit 1;
+fi
+
+echo "deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public
+
+echo "checking out <gh-pages> into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "removing /public"
+rm -rf public/*
+
+echo "generating site"
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
-git push origin gh-pages
+
+echo "updating <gh-pages>"
+cd public && git add --all && git commit -m "new publish" && cd ..
